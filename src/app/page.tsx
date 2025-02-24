ea58fc552267631299af57e40f1c58f7557510de
dacevedo-UMD <dacevedo@terpmail.umd.edu>
2025-02-24 14:18:57 -0500

    added counter functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-import Image from "next/image";
+'use client';
+import { useState } from "react";
 
 export default function Home() {
-  return <p>Hello World!</p>
+const [counter, setCounter] = useState(0)
+
+  return (
+  <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+    <div className="flex flex-row items-center justify-around w-64 h-96 bg-amber-400">
+      <button onClick={() => setCounter(x => x - 1)} className="w-5 h-7 bg-red-600"/>
+      <div className="relative flex flex-col items-center justify-center w-40 h-28 bg-white rounded-md">
+        <h1 className="text-sm absolute top-0 text-red-600 font-bold justify-self-start">Counter</h1>
+        <span className="text-sm text-black font-bold">{counter}</span>
+      </div>
+      <button onClick={() => setCounter(x => x + 1)} className="w-5 h-7 bg-green-400"/>
+    </div>
+  </div>
+  )
 }
